6e15a9a86d7b04728514513b7cac5f025c5258a6
Giancarlos <giancarlos331@gmail.com>
2017-06-19 17:08:41 -0500

    d

---
+++ Sección 1/app.js
@@ -0,0 +1,22 @@
+// let nombre: string = "Giancarlos";
+// let apellido: string = "Gutiérrez";
+// let edad: number = 25;
+// let texto = `Hola, ${ nombre } ${apellido} ${edad}`;
+// //Funciones: Parámetros opcionales, obligarios y por defecto
+// function activar (quien: string, 
+//                   objeto: string = "señal",
+//                   opcional?: string)
+// {
+//     let mensaje: string;
+//     if (opcional) {
+//         mensaje = `${quien} activó la ${objeto} en la ${opcional}`;
+//     } else {
+//         mensaje = `${quien} activó la ${objeto}`; 
+//     }
+// }
+// activar("Gordon");
+// //Funciones por Flecha
+var miFuncion = function (a) {
+    return a;
+};
+var miFuncionF = function (a) { return a; };
