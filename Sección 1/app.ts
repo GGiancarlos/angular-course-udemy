0db005a0eab28fc7703bb54beb0b70cad8861b50
Giancarlos <giancarlos331@gmail.com>
2017-06-19 16:32:36 -0500

    change typescript

--- Sección 1/app.ts
+++ Sección 1/app.ts
@@ -2,4 +2,19 @@ let nombre: string = "Giancarlos";
 let apellido: string = "Gutiérrez";
 let edad: number = 25;
 let texto = `Hola, ${ nombre } ${apellido} ${edad}`;
-console.log(texto);
+
+
+function activar (quien: string, 
+                  objeto: string = "señal",
+                  opcional?: string)
+{
+    let mensaje: string;
+    if (opcional) {
+        mensaje = `${quien} activó la ${objeto} en la ${opcional}`;
+    } else {
+        mensaje = `${quien} activó la ${objeto}`; 
+    }
+
+}
+
+activar("Gordon");
