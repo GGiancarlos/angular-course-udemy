f81acbc266d361118cf532a3cc923f3e70c899f6
Giancarlos <giancarlos331@gmail.com>
2017-06-25 19:47:17 -0500

    Practice, Hero component

--- Section 4/SPA/src/app/components/heroe/heroe.component.ts
+++ Section 4/SPA/src/app/components/heroe/heroe.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HeroesService } from "../../services/heroes.service";
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
+  heroe: any = {};
+  casaDC: string = "assets/img/dc.jpg";
+  casaMarvel: string = "assets/img/marvel.png";
+  constructor(private activatedRoute: ActivatedRoute,
+              private heroService: HeroesService) {
+    this.activatedRoute.params.subscribe(params => {
+      // console.log(params['id']);
+      this.heroe = this.heroService.getHeroe(params['id']);
+      console.log(this.heroe);
+      
+    });
+   }
 
   ngOnInit() {
   }
--- Section 4/SPA/src/app/services/heroes.service.ts
+++ Section 4/SPA/src/app/services/heroes.service.ts
@@ -63,6 +63,10 @@ export class HeroesService {
         return this.heroes;
     }
 
+    getHeroe(id: string): Heroe {
+        return this.heroes[id];
+    }
+
     
 }
 export interface Heroe {
