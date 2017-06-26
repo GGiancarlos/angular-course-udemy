1916af49b4a5fd7ca1945bfe51c60262927b6e2a
Giancarlos <giancarlos331@gmail.com>
2017-06-26 11:16:33 -0500

    Pipes

--- Section 4/SPA/src/app/app.routes.ts
+++ Section 4/SPA/src/app/app.routes.ts
@@ -9,17 +9,11 @@ import { ResultadoComponent } from "./components/resultado/resultado.component";
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'heroe/:id', component: HeroeComponent },
   { path: 'resultado/:termino', component: ResultadoComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
-
-// @NgModule({
-//   imports: [RouterModule.forChild(ROUTES)],
-//   exports: [RouterModule],
-// })
-// export class NameRoutingModule { }
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
